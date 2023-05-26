bddfd48d759aa124c952ee358f22396b278f1225
LEEINWOO <49314154+develinu@users.noreply.github.com>
2023-05-26 18:02:50 +0900

    feature: 달력 그리드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: '인생 달력'),
     );
   }
 }
@@ -68,6 +70,21 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Color getStatusColor(age, currentAge) {
+    var rate = Random().nextInt(100);
+    var isSuccess = rate >= 5;
+
+    if (age < currentAge) {
+      if (isSuccess) {
+        return Colors.green;
+      } else {
+        return Colors.red;
+      }
+    } else {
+      return Colors.white;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -78,41 +95,43 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.count(
+          crossAxisCount: 24 ~/ 2,
+          children: List.generate(960 ~/ 4, (index) {
+            var age = (index ~/ (12 / 4)) + 1;
+            var currentAge = 33;
+            var ageText = index % (24 ~/ 4) == 0 ? "$age" : "";
+
+            return Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  print("Button $index pressed");
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.zero,
+                  backgroundColor: getStatusColor(age, currentAge),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(1.0),
+                    side: BorderSide(
+                        color: age < currentAge ? Colors.grey : Colors.grey
+                    )
+                  )
+                ),
+                child: Text(
+                    ageText,
+                  style: TextStyle(
+                    fontSize: 8.0,
+                  )
+                )
+              ),
+            );
+          })
         ),
       ),
       floatingActionButton: FloatingActionButton(
